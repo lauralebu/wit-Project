3811f737e8841119a454038dc9c6b53f97645c66
lauralebu <47328492+lauralebu@users.noreply.github.com>
2019-03-06 18:08:15 +0300

    Add files via upload

---
+++ laura python/age.py
@@ -0,0 +1,9 @@
+age  = input("enter your age")
+if age < 18:
+  print("you are a minor")
+elif expression:
+  pass age > = 18: ans age <=36:
+  print("you are a youth")
+else:
+    pass print("you are an elder")
+    
---
+++ laura python/dictionary.py
@@ -0,0 +1,7 @@
+def square_of_numbers:
+    squares = []
+start = 1
+while start < 15:
+    squares[start] = start*start
+start = start + 1
+return squares 
